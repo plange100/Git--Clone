7e33d1fb127f43122cc3d118165caeb82826ebf3
Pascal Lange <plange100@gmail.com>
2024-03-31 20:51:35 +0200

    Initial commit

---
+++ recipe.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import mechanicalsoup
+import sqlite3
+
+browser = mechanicalsoup.StatefulBrowser()
+browser.open("https://en.wikipedia.org/wiki/Comparison_of_Linux_distributions")
+
