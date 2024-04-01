2d2e9383f4cb275968b2d533e3f6e1ca7ed3a5d1
Pascal Lange <plange100@gmail.com>
2024-04-01 11:42:11 +0200

    add new csv file

--- COVID19 Analysis/COVID19_Data_Analysis.py
+++ COVID19 Analysis/COVID19_Data_Analysis.py
@@ -1,1 +1,24 @@
 #COVID19 Data analysis
+
+import pandas as pd
+import numpy as npo
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("covid19.csv")
+
+print(df.head(10))
+print(df.info())
+print(df.shape)
+
+#Drop unwanted columns
+#df.drop(["FIPS", "Admin2", "Lat", "Long_", "Last_Update", "Combined_Key"], axis=1, inplace=True)
+# print(df.info())
+
+#Aggregate by country/region using groupby and summing
+#df_sum = df.groupby("Country_Region").sum()
+#print(df_sum)
+#print(df_sum.shape)
+
+#create histogram between various countries
+#df_sum.loc[0].plot()
